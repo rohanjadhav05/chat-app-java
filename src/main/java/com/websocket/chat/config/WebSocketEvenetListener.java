41e1c7178c7d43f27a543d5c9c448daf20ceb94b
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-02-21 19:07:12 +0530

    added the backend API

---
+++ src/main/java/com/websocket/chat/config/WebSocketEvenetListener.java
@@ -0,0 +1,40 @@
+package com.websocket.chat.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.websocket.chat.controller.ChatMessage;
+import com.websocket.chat.controller.MessageType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEvenetListener {
+	
+	@Autowired
+	private final SimpMessageSendingOperations messageTemplate;
+	
+	@EventListener
+	public void handleWebSocketDisconnectListner(SessionDisconnectEvent event) {
+		// TODO 
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String username = (String)headerAccessor.getSessionAttributes().get("username");
+		if(username  != null ){
+			log.info("User disconnected : { "+username+" }");
+			var chatMessage = ChatMessage.builder()
+									.type(MessageType.LEAVE)
+									.sender(username)
+									.build();
+			
+			// which will tell all peoples in chat that particular username has left the chat.
+			messageTemplate.convertAndSend("/topic/public", chatMessage);
+		}
+	}
+}
---
+++ src/main/java/com/websocket/chat/controller/MessageType.java
@@ -0,0 +1,5 @@
+package com.websocket.chat.controller;
+
+public enum MessageType {
+	CHAT, JOIN, LEAVE
+}
